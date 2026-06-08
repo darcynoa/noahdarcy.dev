b78f190d35042573a0673241f6b2b4edd5fbaada
darcynoa <noahdarcy.dev@gmail.com>
2026-06-08 19:48:32 -0400

    font and color setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,19 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Bricolage_Grotesque, Yuji_Mai } from "next/font/google";
+
+const bricolageGrotesque = Bricolage_Grotesque({
+  subsets: ["latin"],
+  variable: "--font-bricolage-grotesque",
+});
+const yujiMai = Yuji_Mai({
+  subsets: ["latin"],
+  weight: ["400"],
+  variable: "--font-yuji-mai",
+});
 
 export const metadata: Metadata = {
-  title: "Coming Soon - noahdarcy.dev",
+  title: "Noah D'Arcy",
   description: "Noah D'Arcy's website is coming soon.",
 };
 
@@ -12,7 +23,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`h-full antialiased`}>
+    <html
+      lang="en"
+      className={`h-full antialiased ${bricolageGrotesque.variable} ${yujiMai.variable}`}
+    >
       <body>{children}</body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div className="w-full h-screen">noahdarcy.dev coming soon</div>;
+  return (
+    <div className="w-full h-screen flex justify-center items-center font-main font-extralight">
+      noahdarcy.dev coming soon
+    </div>
+  );
 }
