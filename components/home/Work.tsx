f993c6bc647954ce2eb28cfd8620bb9fc6c9a813
darcynoa <noahdarcy.dev@gmail.com>
2026-08-12 13:25:10 -0400

    work section mobile styling drafted

--- components/home/Work.tsx
+++ components/home/Work.tsx
@@ -4,6 +4,8 @@ import { useRef, useEffect } from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Image from "next/image";
+import Link from "next/link";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -41,8 +43,31 @@ export default function Work() {
             <h1 className="font-main text-cream text-[5rem] font-thin lg:pl-[2rem] lg:text-[7.5rem]">
                 Work
             </h1>
-            <section className="flex h-screen w-[110%] items-center justify-center pl-[5rem] text-amber-300">
-                Section 1
+            <section className="flex h-screen flex-col items-center justify-center pt-[5rem] lg:w-[110%] lg:pt-0 lg:pl-[5rem]">
+                <h1 className="font-main text-cream z-2 text-center text-[3rem] font-thin lg:text-[7.5rem]">
+                    At The Helm Productions
+                </h1>
+                <div className="-mt-[3.5rem] flex flex-col items-center justify-center gap-[2rem] lg:flex-row">
+                    <Image
+                        src={"/work/athp-2.png"}
+                        alt="One of the many pictures for the At The Helm Productions Project"
+                        width={1000}
+                        height={1000}
+                        className="w-[90%] rounded-[2rem] object-cover opacity-80"
+                    />
+                    <div className="flex flex-col gap-[2rem] px-[1.5rem]">
+                        <p className="font-body text-cream text-[1rem] leading-[1] font-thin">
+                            An astonishing website put together to promote a
+                            budding film company with their first short film
+                        </p>
+                        <Link
+                            href={"mailto:noah@noahdarcy.dev"}
+                            className="bg-red text-cream font-main flex min-w-[20rem] items-center justify-center rounded-[4rem] py-[0.8rem] text-center text-[1.5rem] font-thin uppercase"
+                        >
+                            work with me!
+                        </Link>
+                    </div>
+                </div>
             </section>
             <section className="flex h-screen w-[110%] items-center justify-center text-amber-500">
                 Section 2
